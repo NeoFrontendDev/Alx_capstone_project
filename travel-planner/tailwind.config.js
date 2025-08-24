cec4eeedd81b8585e5b95c0dc747e45a70513b49
Neo Mosebi <neosammosebi01@gmail.com>
2025-08-24 17:23:54 +0200

    Update tailwind.config.js

--- travel-planner/tailwind.config.js
+++ travel-planner/tailwind.config.js
@@ -4,8 +4,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        midnight: '#001F3F',
-        sky: '#87CEEB',
+        white: '#fff',
+        black: '#000',
+        dark_gray_brown: '#231815',
+        dark_purple: '#281a40',
+        dark_gray: '#282828',
+        gray_crimson: '#7b4b55',
+        gray_red: '#b38b85',
+        gray_red: '#b49f9d',
+        gray_orange: '#b88974',
+        pastel_orange: '#f2b893',
+        light_orange: '#f2ebf',
       },
     },
   },
